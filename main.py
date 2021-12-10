22792e8714bd360cf3823e97e0ac745479beb94a
Pierre Zemb <contact@pierrezemb.fr>
2021-12-10 13:08:04 +0100

    Create LICENSE

---
+++ main.py
@@ -0,0 +1,7 @@
+from kafka import KafkaConsumer
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+import json
+
+# On Gitpod, you can click on the Run arrow on the top-right corner
+print("Hello, world!")
